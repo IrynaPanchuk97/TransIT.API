f04c06bcb24d3c1b76886f425ba744825b8698ba
markiyanshvets <markiyan.shvets@gmail.com>
2019-04-02 16:37:03 +0300

    add-template

---
+++ TransIT.DAL/Repositories/Interfaces/IBaseRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TransIT.DAL.Repositories.Interfaces
+{
+    interface IBaseRepository<Entity> where Entity : class
+    {
+    }
+}
---
+++ TransIT.DAL/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TransIT.DAL.UnitOfWork
+{
+    interface IUnitOfWork
+    {
+    }
+}
