ea1eb643cf65a07dab19058647d4898da64e7bf3
Danylo Dudok <danylo.dudok@protonmail.com>
2019-04-02 21:19:40 +0300

    fix TokenDTO

--- TransIT.DAL/Models/DTOs/TokenDTO.cs
+++ TransIT.DAL/Models/DTOs/TokenDTO.cs
@@ -6,7 +6,9 @@ namespace TransIT.DAL.Models.DTOs
     {
         public int Id { get; set; }
         public string RefreshToken { get; set; }
+        public string AccessToken { get; set; }
         public DateTime? CreateDate { get; set; }
         public DateTime? ModDate { get; set; }
+        public UserDTO User { get; set; }
     }
 }
