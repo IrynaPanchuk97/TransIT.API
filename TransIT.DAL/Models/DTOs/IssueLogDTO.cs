0f3f620c2728b8f4936b6a2cffb5cb8b91997833
Danylo Dudok <danylo.dudok@protonmail.com>
2019-04-03 22:35:40 +0300

    DTOs

---
+++ TransIT.DAL/Models/DTOs/IssueLogDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TransIT.DAL.Models.DTOs
+{
+    public class IssueLogDTO
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public decimal? Expenses { get; set; }
+        public StateDTO OldStateId { get; set; }
+        public StateDTO NewStateId { get; set; }
+        public SupplierDTO SupplierId { get; set; }
+        public ActionTypeDTO ActionTypeId { get; set; }
+        public IssueDTO IssueId { get; set; }
+        public UserDTO CreateId { get; set; }
+        public UserDTO ModId { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public DateTime? ModDate { get; set; }
+    }
+}
---
+++ TransIT.DAL/Models/DTOs/MalfunctionGroupDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TransIT.DAL.Models.DTOs
+{
+    public class MalfunctionGroupDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
