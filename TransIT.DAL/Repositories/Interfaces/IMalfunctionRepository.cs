51de2b5fb3ba34a7e5e39fada97962017a4fbf98
markiyanshvets <markiyan.shvets@gmail.com>
2019-04-03 16:23:26 +0300

    Modify

--- TransIT.DAL/Repositories/Interfaces/IMalfunctionRepository.cs
+++ TransIT.DAL/Repositories/Interfaces/IMalfunctionRepository.cs
@@ -6,26 +6,7 @@ using TransIT.DAL.Models;
 
 namespace TransIT.DAL.Repositories.Interfaces
 {
-    class IMalfunctionRepository : IBaseRepository<Malfunction>
+    interface IMalfunctionRepository 
     {
-        public EntityEntry<Malfunction> Add(Malfunction entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<Malfunction> GetAll()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Malfunction GetById(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public EntityEntry<Malfunction> Remove(Malfunction entity)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
