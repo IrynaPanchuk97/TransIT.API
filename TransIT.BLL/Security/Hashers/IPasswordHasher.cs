74bb3acc087250b67dedf796c5454bca5b2ff661
Rostyslav Toch <rokarostuk@gmail.com>
2019-04-04 16:20:58 +0300

    Closes #17 - Create Password Hasher

--- TransIT.BLL/Security/Hashers/IPasswordHasher.cs
+++ TransIT.BLL/Security/Hashers/IPasswordHasher.cs
@@ -1,8 +1,23 @@
 ﻿namespace TransIT.BLL.Security.Hashers
 {
+    /// <summary>
+    /// Interface for classes capable of password hashing
+    /// </summary>
     public interface IPasswordHasher
     {
+        /// <summary>
+        /// Hashes given password and returns the result.
+        /// </summary>
+        /// <param name="password">Password to be hashed</param>
+        /// <returns>Hash of the given password</returns>
         string HashPassword(string password);
+
+        /// <summary>
+        /// Checks if the given password matches the hashed password.
+        /// </summary>
+        /// <param name="password">Given password to be matched</param>
+        /// <param name="hashedPassword">Hashed password to be matched</param>
+        /// <returns>True if passwords matched, else returns false</returns>
         bool CheckMatch(string password, string hashedPassword);
     }
 }
--- TransIT.BLL/Security/Hashers/PasswordHasher.cs
+++ TransIT.BLL/Security/Hashers/PasswordHasher.cs
@@ -6,8 +6,16 @@ using System.Security.Cryptography;
 [assembly: InternalsVisibleTo("TransIT.BLL.Tests")]
 namespace TransIT.BLL.Security.Hashers
 {
+    /// <summary>
+    /// Implementation of IPasswordHasher.
+    /// Class is internal for the assembly.
+    /// </summary>
     internal class PasswordHasher : IPasswordHasher
     {
+        /// <summary>
+        /// <see cref="IPasswordHasher.HashPassword(string)"/>
+        /// </summary>
+        /// <note>This method will return different results for the same password</note>
         public string HashPassword(string password)
         {
             var salt = GenerateSalt(16);
@@ -16,6 +24,9 @@ namespace TransIT.BLL.Security.Hashers
             return $"{Convert.ToBase64String(salt)}:{Convert.ToBase64String(bytes)}";
         }
 
+        /// <summary>
+        /// <see cref="IPasswordHasher.CheckMatch(string, string)"/>
+        /// </summary>
         public bool CheckMatch(string password, string hashedPassword)
         {
             try
@@ -32,6 +43,11 @@ namespace TransIT.BLL.Security.Hashers
             }
         }
 
+        /// <summary>
+        /// Generates salt for hashing algorithm
+        /// </summary>
+        /// <param name="length">Number of bytes in salt</param>
+        /// <returns>Byte array of generated salt</returns>
         private static byte[] GenerateSalt(int length)
         {
             var salt = new byte[length];
