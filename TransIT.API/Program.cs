8866b1cf8141c69feee10d55e0d684b8e54e3973
Danylo Dudok <39445522+danylo-dudok@users.noreply.github.com>
2019-04-06 21:36:02 +0300

    add logging (#28)

--- TransIT.API/Program.cs
+++ TransIT.API/Program.cs
@@ -19,6 +19,11 @@ namespace TransIT.API
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddConsole();
+                });
     }
 }
