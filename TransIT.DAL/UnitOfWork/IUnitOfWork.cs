239a1a6485836db2beaa4ad308d5b3ffe8f253f4
TarasKvasnytsya <tarik12april@gmail.com>
2019-04-07 15:11:00 +0300

    Add properties to IUnitOfWork (#37)

--- TransIT.DAL/UnitOfWork/IUnitOfWork.cs
+++ TransIT.DAL/UnitOfWork/IUnitOfWork.cs
@@ -1,10 +1,27 @@
 ﻿using System;
 using System.Threading.Tasks;
+using TransIT.DAL.Repositories.InterfacesRepositories;
 
 namespace TransIT.DAL.UnitOfWork
 {
     public interface IUnitOfWork 
     {
+        IActionTypeRepository ActionTypeRepository { get; }
+        IBillRepository BillRepository { get; }
+        IDocumentRepository DocumentRepository { get; }
+        IIssueRepository IssueRepository { get; }
+        IIssueLogRepository IssueLogRepository { get; }
+        IMalfunctionRepository MalfunctionRepository { get; }
+        IMalfunctionGroupRepository MalfunctionGroupRepository { get; }
+        IMalfunctionSybgroupRepository MalfunctionSybgroupRepository { get; }
+        IRoleRepository RoleRepository { get; }
+        IUserRepository UserRepository { get; }
+        IStateRepository StateRepository { get; }
+        ISupplierRepository SupplierRepository { get; }
+        IVehicleRepository VehicleRepository { get; }
+        IVehicleTypeRepository VehicleTypeRepository { get; }
+        ITokenRepository TokenRepository { get; }
+
         Task<int> SaveAsync();
     }
 }
