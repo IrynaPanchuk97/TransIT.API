3771cf80b78d9454c441e3752d5c108226dd93e1
Danylo Dudok <danylo.dudok@protonmail.com>
2019-04-07 12:23:56 +0300

    UserService fix

--- TransIT.BLL/Services/IUserService.cs
+++ TransIT.BLL/Services/IUserService.cs
@@ -18,27 +18,27 @@ namespace TransIT.BLL.Services
         /// <summary>
         /// Gets users with pagination
         /// </summary>
-        /// <param name="index">Amount to skip</param>
+        /// <param name="offset">Amount to skip</param>
         /// <param name="amount">Amount to take</param>
         /// <returns>Users</returns>
-        Task<IEnumerable<User>> Get(uint index, uint amount);
+        Task<IEnumerable<User>> Get(uint offset, uint amount);
         /// <summary>
         /// Registers a new user
         /// </summary>
-        /// <param name="value">New user model</param>
+        /// <param name="user">New user model</param>
         /// <returns>Whether operation is successful</returns>
-        Task<bool> Create(User value);
+        Task<User> Create(User user);
         /// <summary>
         /// Updates user
         /// </summary>
-        /// <param name="value">User model to update</param>
+        /// <param name="user">User model to update</param>
         /// <returns>Whether operation is successful</returns>
-        Task<bool> Update(User value);
+        Task<User> Update(User user);
         /// <summary>
         /// Removes user with this id
         /// </summary>
         /// <param name="userId">User id</param>
         /// <returns>Whether operation is successful</returns>
-        Task<bool> Delete(int userId);
+        Task Delete(int userId);
     }
 }
--- TransIT.BLL/Services/UserService.cs
+++ TransIT.BLL/Services/UserService.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using TransIT.BLL.Security.Hashers;
 using TransIT.DAL.Models.Entities;
 using TransIT.DAL.Repositories.InterfacesRepositories;
@@ -16,7 +19,7 @@ namespace TransIT.BLL.Services
         /// <summary>
         /// Saves changes
         /// </summary>
-        private readonly IUnitOfWork _uow;
+        private readonly IUnitOfWork _unitOfWork;
         /// <summary>
         /// User repo
         /// </summary>
@@ -29,24 +32,31 @@ namespace TransIT.BLL.Services
         /// Hasher for password
         /// </summary>
         private readonly IPasswordHasher _hasher;
+        /// <summary>
+        /// Logger
+        /// </summary>
+        private readonly ILogger<UserService> _logger;
         
         /// <summary>
         /// Ctor
         /// </summary>
+        /// <param name="logger">Logs errors</param>
         /// <param name="hasher">Used to hash password</param>
-        /// <param name="uow">Used to save changes</param>
+        /// <param name="unitOfWork">Used to save changes</param>
         /// <param name="userRepository">Used to CRUD users</param>
         /// <param name="roleRepository">Used to retrieve roles</param>
         public UserService(
+            ILogger<UserService> logger,
             IPasswordHasher hasher,
-            IUnitOfWork uow,
+            IUnitOfWork unitOfWork,
             IUserRepository userRepository,
             IRoleRepository roleRepository)
         {
-            _uow = uow;
+            _unitOfWork = unitOfWork;
             _userRepository = userRepository;
             _roleRepository = roleRepository;
             _hasher = hasher;
+            _logger = logger;
         }
 
         /// <summary>
@@ -60,54 +70,79 @@ namespace TransIT.BLL.Services
         /// <summary>
         /// Gives users with pagination
         /// </summary>
-        /// <param name="index">Where to start</param>
+        /// <param name="offset">Where to start</param>
         /// <param name="amount">Amount to give</param>
         /// <returns>List of users</returns>
-        public Task<IEnumerable<User>> Get(uint index, uint amount) =>
-            _userRepository.GetRangeAsync(index, amount);
+        public Task<IEnumerable<User>> Get(uint offset, uint amount) =>
+            _userRepository.GetRangeAsync(offset, amount);
 
         /// <summary>
         /// Creates user if login and password not empty and does not exist in DB
         /// hashes password and set zero to id
         /// </summary>
-        /// <param name="value">User model</param>
+        /// <see cref="IPasswordHasher.HashPassword(string)"/>
+        /// <param name="user">User model</param>
         /// <returns>Is successful</returns>
-        public async Task<bool> Create(User value)
+        public async Task<User> Create(User user)
         {
-            if (string.IsNullOrEmpty(value.Login)
-                || string.IsNullOrEmpty(value.Password)
-                || (await _userRepository.GetAllAsync(u =>
-                u.Login == value.Login)).Any()) 
-                return false;
+            if ((await _userRepository.GetAllAsync(u =>
+                u.Login == user.Login)).Any()) 
+                return null;
             
-            var role = value.Role.Name.ToUpper();
-            value.Role = (await _roleRepository
+            var role = user.Role.Name.ToUpper();
+            user.Role = (await _roleRepository
                 .GetAllAsync(r => r.Name == role))
                 .SingleOrDefault();
 
-            if (value.Role == null) return false;
+            if (user.Role == null) return null;
 
-            value.Id = 0;
-            value.Password = _hasher.HashPassword(value.Password);
-            
-            await _userRepository.AddAsync(value);
-            await _uow.SaveAsync();
-            return true;
+            user.Id = 0;
+            user.Password = _hasher.HashPassword(user.Password);
+
+            try
+            {
+                var res = await _userRepository.AddAsync(user);
+                await _unitOfWork.SaveAsync();
+                return res.Entity;
+            }
+            catch (DbUpdateException e)
+            {
+                _logger.LogError(e, nameof(Create), e.Entries);
+                return null;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, nameof(Create));
+                throw e;
+            }
         }
 
         /// <summary>
         /// Updates user if it is found
         /// </summary>
-        /// <param name="value">User to update</param>
+        /// <param name="user">User to update</param>
         /// <returns>Is successful</returns>
-        public async Task<bool> Update(User value)
+        public async Task<User> Update(User user)
         {
-            if (await _userRepository.GetByIdAsync(value.Id) == null)
-                return false;
+            if (await _userRepository.GetByIdAsync(user.Id) == null)
+                return null;
 
-            _userRepository.Update(value);
-            await _uow.SaveAsync();
-            return true;
+            try
+            {
+                var res = _userRepository.Update(user);
+                await _unitOfWork.SaveAsync();
+                return res.Entity;
+            }
+            catch (DbUpdateException e)
+            {
+                _logger.LogError(e, nameof(Update), e.Entries);
+                return null;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, nameof(Update));
+                throw e;
+            }
         }
 
         /// <summary>
@@ -115,14 +150,22 @@ namespace TransIT.BLL.Services
         /// </summary>
         /// <param name="userId">Id of user to delete</param>
         /// <returns>Is successful</returns>
-        public async Task<bool> Delete(int userId)
+        public async Task Delete(int userId)
         {
-            var user = await _userRepository.GetByIdAsync(userId);
-            if (user == null) return false;
-            
-            _userRepository.Remove(user);
-            await _uow.SaveAsync();
-            return true;
+            try
+            {
+                _userRepository.Remove(new User {Id = userId});
+                await _unitOfWork.SaveAsync();
+            }
+            catch (DbUpdateException e)
+            {
+                _logger.LogError(e, nameof(Delete), e.Entries);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, nameof(Delete));
+                throw e;
+            }
         }
     }
 }
