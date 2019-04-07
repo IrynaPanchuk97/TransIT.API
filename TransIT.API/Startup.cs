ee49e9595fcb1f0ead2ba4b2ec34874de25e1f91
Danylo Dudok <danylo.dudok@protonmail.com>
2019-04-07 12:35:01 +0300

    add userservice to di

--- TransIT.API/Startup.cs
+++ TransIT.API/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using TransIT.BLL.Services;
 
 namespace TransIT.API
 {
@@ -25,6 +26,12 @@ namespace TransIT.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            #region Services
+
+            services.AddScoped<IUserService, UserService>();
+
+            #endregion
+            
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
