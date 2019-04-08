20a461f0fe3288175125cf5112dd88a4c827a00e
Rostyslav Toch <rokarostuk@gmail.com>
2019-04-08 15:36:38 +0300

    Added generic unit tests

---
+++ TransIT.BLL.Tests/Services/CrudServiceTests.cs
@@ -0,0 +1,272 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Internal;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TransIT.BLL.Services;
+using TransIT.DAL.Models.Entities;
+using TransIT.DAL.Models.Entities.Abstractions;
+using TransIT.DAL.Repositories;
+using TransIT.DAL.UnitOfWork;
+using Xunit;
+
+namespace TransIT.BLL.Tests.Services
+{
+    public abstract class CrudServiceTest<TEntity> where TEntity : class, IEntity, new()
+    {
+        protected Mock<IUnitOfWork> _unitOfWork;
+        protected Mock<IBaseRepository<TEntity>> _repository;
+        protected Mock<ILogger<CrudService<TEntity>>> _logger;
+        protected CrudService<TEntity> _crudService;
+        protected List<TEntity> _context;
+        protected int _idCounter;
+        public CrudServiceTest()
+        {
+            InitializeMocks();
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        public async Task GetAsync_GivenValidId_ReturnsGroup(int id)
+        {
+            var result = await _crudService.GetAsync(id);
+
+            Assert.Equal(result, _context[id - 1]);
+        }
+
+        [Fact]
+        public async Task GetAsync_GivenInvalidId_ReturnsNull()
+        {
+            int id = 0;
+
+            var result = await _crudService.GetAsync(id);
+
+            Assert.Null(result);
+        }
+
+        [Theory]
+        [InlineData(0, 4)]
+        [InlineData(2, 2)]
+        [InlineData(3, 2)]
+        [InlineData(5, 3)]
+        [InlineData(0, 0)]
+        public async Task GetAllAsync_GivenOffsetAndSize_ReturnsAllGroupsInRange(uint offset, uint size)
+        {
+            var result = await _crudService.GetRangeAsync(offset, size);
+
+            Assert.Equal(result, _context.Skip((int)offset).Take((int)size));
+        }
+
+        [Theory]
+        [MemberData(nameof(SampleData))]
+        public async Task CreateAsync_GivenMalfunctionGroup_AddsGroupToContext(TEntity entity)
+        {
+            await _crudService.CreateAsync(entity);
+
+            Assert.Contains(entity, _context);
+            Assert.Equal(entity.Id, _idCounter - 1);
+            _repository.Verify(r => r.AddAsync(entity), Times.Once);
+            _unitOfWork.Verify(u => u.SaveAsync(), Times.Once);
+        }
+
+        [Fact]
+        public async Task CreateAsync_GivenWrongGroup_ReturnsNull()
+        {
+            var entity = new TEntity { Id = 1 };
+            var exception = new DbUpdateException("", null as Exception);
+            _repository.Setup(r => r.AddAsync(entity)).Throws(exception);
+
+            var result = await _crudService.CreateAsync(entity);
+
+            Assert.Null(result);
+            _repository.Verify(r => r.AddAsync(entity), Times.Once);
+            VerifyLogger();
+        }
+
+        [Fact]
+        public async Task CreateAsync_GivenNull_ThrowsException()
+        {
+            await Assert.ThrowsAsync<NullReferenceException>(async () => await _crudService.CreateAsync(null));
+            VerifyLogger();
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        public async Task UpdateAsync_GivenExistingItem_ReturnsUpdatedItem(int id)
+        {
+            var entity = new TEntity { Id = id };
+            int previousCount = _context.Count;
+
+            var result = await _crudService.UpdateAsync(entity);
+
+            Assert.Equal(result, entity);
+            Assert.True(_context.Count == previousCount);
+            _repository.Verify(r => r.Update(entity), Times.Once);
+            _unitOfWork.Verify(u => u.SaveAsync(), Times.Once);
+        }
+
+        [Fact]
+        public async Task UpdateAsync_GivenWrongGroup_ReturnsNull()
+        {
+            var entity = new TEntity { Id = 1 };
+            var exception = new DbUpdateException("", null as Exception);
+            _repository.Setup(r => r.Update(entity)).Throws(exception);
+
+            var result = await _crudService.UpdateAsync(entity);
+
+            Assert.Null(result);
+            _repository.Verify(r => r.Update(entity), Times.Once);
+            VerifyLogger();
+        }
+
+        [Fact]
+        public async Task UpdateAsync_GivenNull_ThrowsException()
+        {
+            await Assert.ThrowsAsync<NullReferenceException>(async () => await _crudService.UpdateAsync(null));
+            VerifyLogger();
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        public async Task DeleteAsync_GivenExistingItemId_DeletesItem(int id)
+        {
+            int previousCount = _context.Count;
+
+            await _crudService.DeleteAsync(id);
+
+            var entity = _context.Find(e => e.Id == id);
+            Assert.Null(entity);
+            Assert.True(_context.Count == previousCount - 1);
+            _repository.Verify(r => r.Remove(It.IsAny<TEntity>()), Times.Once);
+            _unitOfWork.Verify(u => u.SaveAsync(), Times.Once);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(100)]
+        public async Task DeleteAsync_GivenNonExistingItemId_ReturnsNothing(int id)
+        {
+            int previousCount = _context.Count;
+
+            await _crudService.DeleteAsync(id);
+
+            Assert.True(_context.Count == previousCount);
+            _repository.Verify(r => r.Remove(It.IsAny<TEntity>()), Times.Once);
+            VerifyLogger();
+        }
+
+        [Fact]
+        public async Task DeleteAsync_GivenExceptionInRepository_ReturnsNull()
+        {
+            var exception = new DbUpdateException("", null as Exception);
+            _repository.Setup(r => r.Remove(It.IsAny<TEntity>())).Throws(exception);
+
+            await _crudService.DeleteAsync(3);
+
+            _repository.Verify(r => r.Remove(It.IsAny<TEntity>()), Times.Once);
+            VerifyLogger();
+        }
+
+        [Fact]
+        public async Task DeleteAsync_GivenExceptionInRepository_ThrowsException()
+        {
+            _repository.Setup(r => r.Remove(It.IsAny<TEntity>())).Throws(new Exception());
+            await Assert.ThrowsAsync<Exception>(async () => await _crudService.DeleteAsync(0));
+            VerifyLogger();
+        }
+
+        protected void InitializeMocks()
+        {
+            InitializeContext();
+            InitializeRepositoryMock();
+            InitializeUnitOfWork();
+            InitializeLogger();
+            InitializeService();
+        }
+
+        protected void InitializeContext()
+        {
+            _idCounter = 5;
+            _context = new List<TEntity>
+            {
+                new TEntity { Id = 1 },
+                new TEntity { Id = 2 },
+                new TEntity { Id = 3 },
+                new TEntity { Id = 4 },
+            };
+        }
+
+        protected void InitializeRepositoryMock()
+        {
+            _repository = new Mock<IBaseRepository<TEntity>>();
+
+            _repository.Setup(r => r.GetAllAsync()).ReturnsAsync(_context);
+
+            _repository.Setup(r => r.GetByIdAsync(It.IsAny<int>()))
+                .ReturnsAsync((int id) => _context.Find(g => g.Id == id));
+
+            _repository.Setup(r => r.GetRangeAsync(It.IsAny<uint>(), It.IsAny<uint>()))
+                .Returns<uint, uint>((offset, size) => Task.FromResult(_context.Skip((int)offset).Take((int)size)));
+
+            _repository.Setup(r => r.AddAsync(It.IsAny<TEntity>()))
+                .Callback<TEntity>(entity =>
+                {
+                    entity.Id = _idCounter++;
+                    _context.Add(entity);
+                })
+                .ReturnsAsync((TEntity entity) => entity);
+
+            _repository.Setup(r => r.Remove(It.IsAny<TEntity>()))
+                .Callback<TEntity>(entity =>
+                {
+                    var founded = _context.Find(g => g.Id == entity.Id);
+                    if (founded == null)
+                    {
+                        throw new DbUpdateException("", new Exception());
+                    }
+                    _context.Remove(founded);
+                });
+
+            _repository.Setup(r => r.Update(It.IsAny<TEntity>()))
+                .Callback<TEntity>(entity => _context[_context.FindIndex(e => e.Id == entity.Id)] = entity);
+        }
+
+        protected void InitializeUnitOfWork()
+        {
+            _unitOfWork = new Mock<IUnitOfWork>();
+            _unitOfWork.Setup(u => u.SaveAsync()).ReturnsAsync(_context.Count);
+        }
+
+        protected void InitializeLogger()
+        {
+            _logger = new Mock<ILogger<CrudService<TEntity>>>();
+        }
+
+        protected abstract void InitializeService();
+
+        protected void VerifyLogger()
+        {
+            _logger.Verify(x => x.Log(LogLevel.Error, It.IsAny<EventId>(), It.IsAny<FormattedLogValues>(), It.IsAny<Exception>(), It.IsAny<Func<object, Exception, string>>()), Times.Once);
+        }
+
+        public static IEnumerable<object[]> SampleData =>
+            new[]
+            {
+                new object [] { new MalfunctionGroup { Id = 2, Name = "Roka" } },
+                new object [] { new MalfunctionGroup { Id = 0, Name = "TEst" } },
+                new object [] { new MalfunctionGroup { Id = 5, Name = "Example" } }
+            };
+    }
+}
