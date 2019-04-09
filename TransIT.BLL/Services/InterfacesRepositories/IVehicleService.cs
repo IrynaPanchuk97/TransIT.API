f34970e953757e286c2d7474f9ecf335f1b16fe1
TarasKvasnytsya <tarik12april@gmail.com>
2019-04-09 10:38:44 +0300

    Create Vehicle service (#67)

---
+++ TransIT.BLL/Services/InterfacesRepositories/IVehicleService.cs
@@ -0,0 +1,11 @@
+﻿using TransIT.DAL.Models.Entities;
+
+namespace TransIT.BLL.Services.InterfacesRepositories
+{
+    /// <summary>
+    /// Interface for Vehicle service
+    /// </summary>
+    public interface IVehicleService : ICrudService<Vehicle>
+    {
+    }
+}
