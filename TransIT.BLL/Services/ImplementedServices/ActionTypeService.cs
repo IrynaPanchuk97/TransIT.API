5a33a207b7156ed38a85327ac1de0334e1c71591
Danylo Dudok <danylo.dudok@protonmail.com>
2019-04-09 18:53:49 +0300

    action type search

--- TransIT.BLL/Services/ImplementedServices/ActionTypeService.cs
+++ TransIT.BLL/Services/ImplementedServices/ActionTypeService.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using TransIT.BLL.Services.InterfacesRepositories;
 using TransIT.DAL.Models.Entities;
 using TransIT.DAL.Repositories.InterfacesRepositories;
@@ -23,5 +25,12 @@ namespace TransIT.BLL.Services.ImplementedServices
             IUnitOfWork unitOfWork,
             ILogger<CrudService<ActionType>> logger,
             IActionTypeRepository repository) : base(unitOfWork, logger, repository) { }
+
+        public override Task<IEnumerable<ActionType>> SearchAsync(string search)
+        {
+            search = search.ToUpperInvariant();
+            return _unitOfWork.ActionTypeRepository.GetAllAsync(a =>
+                a.Name.ToUpperInvariant() == search);
+        }
     }
 }
