ec0fd2a6fa0efd7bd93d8a6ef2e48671e8fb4e43
Danylo Dudok <danylo.dudok@protonmail.com>
2019-04-09 19:33:49 +0300

    add services

---
+++ TransIT.BLL/Services/ImplementedServices/IssueService.cs
@@ -0,0 +1,27 @@
+using Microsoft.Extensions.Logging;
+using TransIT.BLL.Services.InterfacesRepositories;
+using TransIT.DAL.Models.Entities;
+using TransIT.DAL.Repositories.InterfacesRepositories;
+using TransIT.DAL.UnitOfWork;
+
+namespace TransIT.BLL.Services.ImplementedServices
+{
+    /// <summary>
+    /// Malfunction Group CRUD service
+    /// </summary>
+    /// <see cref="IIssueService"/>
+    public class IssueService : CrudService<Issue>, IIssueService
+    {
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        /// <param name="unitOfWork">Unit of work pattern</param>
+        /// <param name="logger">Log on error</param>
+        /// <param name="repository">CRUD operations on entity</param>
+        /// <see cref="CrudService{TEntity}"/>
+        public IssueService(
+            IUnitOfWork unitOfWork,
+            ILogger<CrudService<Issue>> logger,
+            IIssueRepository repository) : base(unitOfWork, logger, repository) { }
+    }
+}
---
+++ TransIT.BLL/Services/ImplementedServices/SupplierService.cs
@@ -0,0 +1,27 @@
+using Microsoft.Extensions.Logging;
+using TransIT.BLL.Services.InterfacesRepositories;
+using TransIT.DAL.Models.Entities;
+using TransIT.DAL.Repositories.InterfacesRepositories;
+using TransIT.DAL.UnitOfWork;
+
+namespace TransIT.BLL.Services.ImplementedServices
+{
+    /// <summary>
+    /// Malfunction Group CRUD service
+    /// </summary>
+    /// <see cref="ISupplierService"/>
+    public class SupplierService : CrudService<Supplier>, ISupplierService
+    {
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        /// <param name="unitOfWork">Unit of work pattern</param>
+        /// <param name="logger">Log on error</param>
+        /// <param name="repository">CRUD operations on entity</param>
+        /// <see cref="CrudService{TEntity}"/>
+        public SupplierService(
+            IUnitOfWork unitOfWork,
+            ILogger<CrudService<Supplier>> logger,
+            ISupplierRepository repository) : base(unitOfWork, logger, repository) { }
+    }
+}
