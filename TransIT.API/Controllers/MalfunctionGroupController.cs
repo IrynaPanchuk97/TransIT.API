227af99c79edb6e51ec872c8ea1753a1cbc06c6d
Maksym Yaremko <maksym.yaremko@gmail.com>
2019-04-10 17:55:31 +0300

    Malfunction group api

---
+++ TransIT.API/Controllers/MalfunctionGroupController.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TransIT.BLL.Services.InterfacesRepositories;
+using TransIT.DAL.Models.DTOs;
+using TransIT.DAL.Models.Entities;
+
+namespace TransIT.API.Controllers
+{
+    [Authorize(Roles = "ADMIN,WORKER,ENGINEER,CUSTOMER,ANALYST")]
+    public class MalfunctionGroupController : DataController<MalfunctionGroup, MalfunctionGroupDTO>
+    {
+        private IMalfunctionGroupService _malfunctionGroupService;
+
+        public MalfunctionGroupController(IMapper mapper, IMalfunctionGroupService malfunctionGroupService) : base(mapper, malfunctionGroupService)
+        {
+            _malfunctionGroupService = malfunctionGroupService;
+        }
+    }
+}
