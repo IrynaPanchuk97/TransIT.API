7e63d19460fb295c08061818de5fa33fd11c25e7
Maksym Yaremko <maksym.yaremko@gmail.com>
2019-04-10 18:43:11 +0300

    remove usings

--- TransIT.API/Controllers/MalfunctionSubGroupController.cs
+++ TransIT.API/Controllers/MalfunctionSubGroupController.cs
@@ -1,9 +1,6 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using TransIT.BLL.Services.ImplementedServices;
 using TransIT.DAL.Models.DTOs;
