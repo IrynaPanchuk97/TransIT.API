49f5d8ae6ad01dfb9a8cd442465f4b31c44fde76
Taras Kvasnytsya <taras1kvasnytsya@gmail.com>
2019-04-10 18:04:21 +0300

    Add vehicle type controller

---
+++ TransIT.API/Controllers/VehicleTypeController.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using TransIT.BLL.Services;
+using TransIT.DAL.Models.DTOs;
+using TransIT.DAL.Models.Entities;
+
+
+namespace TransIT.API.Controllers
+{
+    [Authorize(Roles = "ADMIN,ENGINEER,ANALYST")]
+    public class VehicleTypeController : DataController<VehicleType, VehicleTypeDTO>
+    {
+        private readonly IVehicleTypeService _vehicleTypeService;
+
+        public VehicleTypeController(IMapper mapper, IVehicleTypeService vehicleTypeService) : base(mapper, vehicleTypeService)
+        {
+            _vehicleTypeService = vehicleTypeService;
+        }
+    }
+}
