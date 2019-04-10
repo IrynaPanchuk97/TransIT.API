19594259c7d6b0007c31424fbb3480b66dec1291
Taras Kvasnytsya <taras1kvasnytsya@gmail.com>
2019-04-10 18:31:04 +0300

    Add vehicle type controller

--- TransIT.API/Controllers/VehicleTypeController.cs
+++ TransIT.API/Controllers/VehicleTypeController.cs
@@ -1,5 +1,7 @@
-﻿using AutoMapper;
+﻿using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using TransIT.BLL.Services;
 using TransIT.DAL.Models.DTOs;
 using TransIT.DAL.Models.Entities;
@@ -7,7 +9,7 @@ using TransIT.DAL.Models.Entities;
 
 namespace TransIT.API.Controllers
 {
-    [Authorize(Roles = "ADMIN,ENGINEER,ANALYST")]
+    [Authorize(Roles = "ADMIN")]
     public class VehicleTypeController : DataController<VehicleType, VehicleTypeDTO>
     {
         private readonly IVehicleTypeService _vehicleTypeService;
@@ -15,6 +17,24 @@ namespace TransIT.API.Controllers
         public VehicleTypeController(IMapper mapper, IVehicleTypeService vehicleTypeService) : base(mapper, vehicleTypeService)
         {
             _vehicleTypeService = vehicleTypeService;
+        }
+
+        [Authorize(Roles = "ADMIN,ENGINEER,CUSTOMER,ANALYST,WORKER")]
+        public override Task<IActionResult> Get(int id)
+        {
+            return base.Get(id);
+        }
+
+        [Authorize(Roles = "ADMIN,ENGINEER,CUSTOMER,ANALYST,WORKER")]
+        public override Task<IActionResult> Get([FromQuery] string search)
+        {
+            return base.Get(search);
+        }
+
+        [Authorize(Roles = "ADMIN,ENGINEER,CUSTOMER,ANALYST,WORKER")]
+        public override Task<IActionResult> Get([FromQuery] uint offset, uint amount)
+        {
+            return base.Get(offset, amount);
         }
     }
 }
