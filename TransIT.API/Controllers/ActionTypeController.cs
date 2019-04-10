29cc46eb9457f1ad061efc6e0b2890d3b0e77e38
markiyanshvets <markiyan.shvets@gmail.com>
2019-04-10 17:48:52 +0300

    action type controller

---
+++ TransIT.API/Controllers/ActionTypeController.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using TransIT.BLL.Services.InterfacesRepositories;
+using TransIT.DAL.Models.DTOs;
+using TransIT.DAL.Models.Entities;
+
+namespace TransIT.API.Controllers
+{
+    [Authorize(Roles = "ADMIN,ENGINEER,ANALYST")]
+    public class ActionTypeController : DataController<ActionType, ActionTypeDTO>
+    {
+        private readonly IActionTypeService _actionTypeService;
+
+        public ActionTypeController(IMapper mapper, IActionTypeService actionType) : base(mapper, actionType)
+        {
+            _actionTypeService = actionType;
+        }
+    }
+}
