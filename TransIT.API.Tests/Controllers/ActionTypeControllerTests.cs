584ab953934b4607a5ad74488def66b57b03747e
markiyanshvets <markiyan.shvets@gmail.com>
2019-04-11 19:09:25 +0300

    Create tests project

---
+++ TransIT.API.Tests/Controllers/ActionTypeControllerTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using TransIT.API.Controllers;
+
+namespace TransIT.API.Tests.Controllers
+{
+    [TestFixture]
+    public class ActionTypeControllerTests
+    {
+        public ActionTypeControllerTests()
+        {
+        }
+    }
+}
