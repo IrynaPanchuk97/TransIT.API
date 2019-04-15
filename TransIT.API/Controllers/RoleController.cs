13c32c2750544a950da69f8e406ba20e851f6c8f
markiyanshvets <markiyan.shvets@gmail.com>
2019-04-15 10:54:05 +0300

    controller att

--- TransIT.API/Controllers/RoleController.cs
+++ TransIT.API/Controllers/RoleController.cs
@@ -1,14 +1,9 @@
-using System;
 using System.Threading.Tasks;
 using AutoMapper;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using TransIT.BLL.Services.InterfacesRepositories;
-using TransIT.DAL.Models.DTOs;
 using TransIT.DAL.Models.Entities;
-using TransIT.DAL.Repositories.InterfacesRepositories;
 
 namespace TransIT.API.Controllers
 {
