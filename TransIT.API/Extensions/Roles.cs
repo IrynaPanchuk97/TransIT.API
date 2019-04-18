988db5afa0c5a32577f890ca5c123f483a5226b5
Danylo Dudok <danylo.dudok@protonmail.com>
2019-04-18 19:58:11 +0300

    issue endpoint

---
+++ TransIT.API/Extensions/Roles.cs
@@ -0,0 +1,11 @@
+namespace TransIT.API.Extensions
+{
+    public static class ROLE
+    {
+        public const string CUSTOMER = "CUSTOMER";
+        public const string ENGINEER = "ENGINEER";
+        public const string ADMIN = "ADMIN";
+        public const string ANALYST = "ANALYST";
+        public const string WORKER = "WORKER";
+    }
+}
