9df3f580105636744e49537e7cce32ae8084dc90
Danylo Dudok <danylo.dudok@protonmail.com>
2019-04-21 20:34:02 +0300

    fix mappings

--- TransIT.DAL/Models/Mappings/IssueProfile.cs
+++ TransIT.DAL/Models/Mappings/IssueProfile.cs
@@ -1,4 +1,5 @@
 using AutoMapper;
+using Microsoft.Extensions.Options;
 using TransIT.DAL.Models.DTOs;
 using TransIT.DAL.Models.Entities;
 
@@ -9,17 +10,18 @@ namespace TransIT.DAL.Models.Mappings
         public IssueProfile()
         {
             CreateMap<IssueDTO, Issue>()
-                .ForMember(i => i.AssignedToNavigation, opt => opt.MapFrom(i => i.AssignedTo))
+                .ForMember(i => i.AssignedToNavigation, opt => opt.Ignore())
                 .ForMember(i => i.ModId, opt => opt.Ignore())
                 .ForMember(i => i.CreateId, opt => opt.Ignore())
                 .ForMember(i => i.Mod, opt => opt.Ignore())
                 .ForMember(i => i.Create, opt => opt.Ignore())
                 .ForMember(i => i.Bill, opt => opt.Ignore())
                 .ForMember(i => i.IssueLog, opt => opt.Ignore())
-                .ForMember(i => i.StateId, opt => opt.Ignore())
+                .ForMember(i => i.StateId, opt => opt.MapFrom(x => x.State.Id))
+                .ForMember(i => i.State, opt => opt.Ignore())
                 .ForMember(i => i.VehicleId, opt => opt.MapFrom(d => d.Vehicle == null ? 0 : d.Vehicle.Id))
                 .ForMember(i => i.MalfunctionId, opt => opt.MapFrom(d => d.Malfunction == null ? 0 : d.Malfunction.Id))
-                .ForMember(i => i.AssignedTo, opt => opt.Ignore());
+                .ForMember(i => i.AssignedTo, opt => opt.MapFrom(i => i.AssignedTo.Id));
             CreateMap<Issue, IssueDTO>();
 
         }
--- TransIT.DAL/Models/Mappings/VehicleProfile.cs
+++ TransIT.DAL/Models/Mappings/VehicleProfile.cs
@@ -15,7 +15,8 @@ namespace TransIT.DAL.Models.Mappings
                 .ForMember(v => v.Create, opt => opt.Ignore())
                 .ForMember(v => v.Issue, opt => opt.Ignore())
                 .ForMember(v => v.InventoryId, opt => opt.Ignore())
-                .ForMember(v => v.VehicleTypeId, opt => opt.Ignore())
+                .ForMember(v => v.VehicleTypeId, opt => opt.MapFrom(x => x.VehicleType.Id))
+                .ForMember(v => v.VehicleType, opt => opt.Ignore())
                 .ForMember(v => v.ModDate, opt => opt.Ignore())
                 .ForMember(v => v.CreateDate, opt => opt.Ignore());
             CreateMap<Vehicle, VehicleDTO>();
