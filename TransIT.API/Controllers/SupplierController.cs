5633d972da0cc39d2a2c1d96b04ecfdafe1c3f65
Danylo Dudok <39445522+danylo-dudok@users.noreply.github.com>
2019-04-24 20:30:46 +0300

    Service manager (#123)
    * delete constraint
    
    * Fix bugs in issue controller
    
    * fix models
    
    * fix dto
    
    * user model
    
    * fix vehicle
    
    * fix vehicle config
    
    * issueLog by issue id
    
    * fix user
    
    * Update appsettings.Development.json
    
    * issue endpoint
    
    * role is required
    
    * fix
    
    * admin udpate user
    
    * fix
    
    * Update IUserRepository.cs
    
    * Update UserRepository.cs
    
    * fix issuelog
    
    * issue log
    
    * fix mappings
    
    * fix
    
    * fix
    
    * fix issuelog
    
    * provide get assignees
    
    * add user id assigning & documnets endpoint
    
    * fix assigning user for issue
    
    * fix issue
    
    * fix issue
    
    * fix cyrilic text
    
    * fix issuelog
    
    * fix supplier controller

--- TransIT.API/Controllers/SupplierController.cs
+++ TransIT.API/Controllers/SupplierController.cs
@@ -1,12 +1,14 @@
+using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using TransIT.BLL.Services.InterfacesRepositories;
 using TransIT.DAL.Models.DTOs;
 using TransIT.DAL.Models.Entities;
 
 namespace TransIT.API.Controllers
 {
-    [Authorize(Roles = "ENGINEER,CUSTOMER,ANALYST")]
+    [Authorize(Roles = "ADMIN,ENGINEER,CUSTOMER,ANALYST")]
     public class SupplierController : DataController<Supplier, SupplierDTO>
     {
         private readonly ISupplierService _supplierService;
@@ -14,6 +16,27 @@ namespace TransIT.API.Controllers
         public SupplierController(IMapper mapper, ISupplierService supplierService) : base(mapper, supplierService)
         {
             _supplierService = supplierService;
+        }
+        
+        [HttpPost]
+        [Authorize(Roles = "ADMIN")]
+        public override Task<IActionResult> Create([FromBody] SupplierDTO obj)
+        {
+            return base.Create(obj);
+        }
+
+        [HttpPut("{id}")]
+        [Authorize(Roles = "ADMIN")]
+        public override Task<IActionResult> Update(int id, [FromBody] SupplierDTO obj)
+        {
+            return base.Update(id, obj);
+        }
+
+        [HttpDelete("{id}")]
+        [Authorize(Roles = "ADMIN")]
+        public override Task<IActionResult> Delete(int id)
+        {
+            return base.Delete(id);
         }
     }
 }
