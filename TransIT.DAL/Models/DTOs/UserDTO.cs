f848af95761d5d1b3b419bacd509eb561b4b7c1a
Danylo Dudok <danylo.dudok@protonmail.com>
2019-05-03 13:58:04 +0300

    isactive for user

--- TransIT.DAL/Models/DTOs/UserDTO.cs
+++ TransIT.DAL/Models/DTOs/UserDTO.cs
@@ -6,6 +6,7 @@
         public string FirstName { get; set; }
         public string MiddleName { get; set; }
         public string LastName { get; set; }
+        public bool IsActive { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public string Login { get; set; }
