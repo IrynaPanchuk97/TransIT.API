12de9ad7dc1424a3ad274d43b218df4cb2a279a7
Rostyslav <RokaRostuk@gmail.com>
2019-05-05 11:42:47 +0300

    Non required id for DTO

--- TransIT.DAL/Models/DTOs/StateDTO.cs
+++ TransIT.DAL/Models/DTOs/StateDTO.cs
@@ -2,7 +2,7 @@
 {
     public class StateDTO
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public string Name { get; set; }
         public string TransName { get; set; }
     }
