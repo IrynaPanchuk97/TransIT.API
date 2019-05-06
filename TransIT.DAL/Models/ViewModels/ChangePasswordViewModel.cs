b1fe377e705fa1f375add2c8a36b69fc3229574d
Rostyslav Toch <rokarostuk@gmail.com>
2019-05-06 13:35:21 +0300

    Added validator

--- TransIT.DAL/Models/ViewModels/ChangePasswordViewModel.cs
+++ TransIT.DAL/Models/ViewModels/ChangePasswordViewModel.cs
@@ -4,9 +4,6 @@ namespace TransIT.DAL.Models.ViewModels
 {
     public class ChangePasswordViewModel
     {
-        [Required]
-        [MinLength(6)]
-        [MaxLength(30)]
         public string Password { get; set; }
     }
 }
