b7f1e772433022dca11cd51606107d7a3b24f2fe
Iryna Panchuk <rusa1234527@gmail.com>
2019-05-06 13:36:03 +0300

    add pass hasher if update pass (#134)
    * add pass hasher if update pass
    
    * Change PAssword
    
    * Closes #134 - Change Password For User
    
    * Added validator

---
+++ TransIT.DAL/Models/Validations/ChangePasswordValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using TransIT.DAL.Models.ViewModels;
+
+namespace TransIT.DAL.Models.Validations
+{
+    public class ChangePasswordValidator : AbstractValidator<ChangePasswordViewModel>
+    {
+        public ChangePasswordValidator()
+        {
+            RuleFor(p => p.Password)
+                .NotNull()
+                .NotEmpty()
+                .MinimumLength(6)
+                .MaximumLength(30);
+        }
+    }
+}
