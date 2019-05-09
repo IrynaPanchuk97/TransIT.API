d517dce0fc1e4bfc6ed780f252f1bc8f9bd43061
Danylo Dudok <39445522+danylo-dudok@users.noreply.github.com>
2019-05-09 17:07:12 +0300

    Document include fix (#139)
    * delete constraint
    
    * Fix bugs in issue controller
    
    * fix models
    
    * fix dto
    
    * user model
    
    * filtering and paging
    
    * include extend
    
    * Delete TransIT.BLL.xml

--- TransIT.DAL/Repositories/ImplementedRepositories/DocumentRepository.cs
+++ TransIT.DAL/Repositories/ImplementedRepositories/DocumentRepository.cs
@@ -8,13 +8,31 @@ namespace TransIT.DAL.Repositories.ImplementedRepositories
     public class DocumentRepository : BaseRepository<Document>, IDocumentRepository
     {
         public DocumentRepository(DbContext context)
-               : base(context)
+            : base(context)
         {
         }
 
-        protected override IQueryable<Document> ComplexEntities => Entities.
-           Include(t => t.Create).
-           Include(z => z.IssueLog).          
-           Include(a => a.Mod).OrderByDescending(u => u.ModDate).ThenByDescending(x => x.CreateDate);
+        protected override IQueryable<Document> ComplexEntities => Entities
+            .Include(t => t.Create)
+            .Include(z => z.IssueLog)
+            .ThenInclude(x => x.Issue.State)
+            .Include(x => x.IssueLog)
+            .ThenInclude(x => x.Issue.Vehicle)
+            .ThenInclude(x => x.VehicleType)
+            .Include(z => z.IssueLog)
+            .ThenInclude(x => x.Supplier)
+            .Include(z => z.IssueLog)
+            .ThenInclude(x => x.ActionType)
+            .Include(z => z.IssueLog)
+            .ThenInclude(x => x.Issue.Malfunction)
+            .Include(z => z.IssueLog)
+            .ThenInclude(x => x.NewState)
+            .Include(z => z.IssueLog)
+            .ThenInclude(x => x.OldState)
+            .Include(z => z.IssueLog)
+            .ThenInclude(x => x.Issue.AssignedToNavigation)
+            .Include(a => a.Mod)
+            .OrderByDescending(u => u.ModDate)
+            .ThenByDescending(x => x.CreateDate);
     }
 }
