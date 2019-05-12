bbfc4125cd15fade010a3b149b75d7f11d988f87
markiyanshvets <markiyan.shvets@gmail.com>
2019-05-12 17:41:05 +0300

    Controllers for country and currency

---
+++ TransIT.API/Controllers/CountryController.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TransIT.BLL.Services;
+using TransIT.BLL.Services.Interfaces;
+using TransIT.DAL.Models.DTOs;
+using TransIT.DAL.Models.Entities;
+
+namespace TransIT.API.Controllers
+{
+    [Authorize(Roles = "ADMIN,ENGINEER,CUSTOMER,ANALYST")]
+    public class CountryController : DataController<Country, CountryDTO>
+    {
+        private readonly ICountryService _countryService;
+
+        public CountryController(
+            IMapper mapper,
+            ICountryService countryService,
+            IODCrudService<Country> odService
+            ) : base(mapper, countryService, odService)
+        {
+            _countryService = countryService;
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "ADMIN")]
+        public override Task<IActionResult> Create([FromBody] CountryDTO obj)
+        {
+            return base.Create(obj);
+        }
+
+        [HttpPut("{id}")]
+        [Authorize(Roles = "ADMIN")]
+        public override Task<IActionResult> Update(int id, [FromBody] CountryDTO obj)
+        {
+            return base.Update(id, obj);
+        }
+
+        [HttpDelete("{id}")]
+        [Authorize(Roles = "ADMIN")]
+        public override Task<IActionResult> Delete(int id)
+        {
+            return base.Delete(id);
+        }
+    }
+}
