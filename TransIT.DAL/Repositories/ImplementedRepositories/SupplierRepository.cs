a6f0db12edbe7966c8344d73eed8c0e03124de5e
markiyanshvets <markiyan.shvets@gmail.com>
2019-05-12 17:12:14 +0300

    finish DAL lvl for country and currency

--- TransIT.DAL/Repositories/ImplementedRepositories/SupplierRepository.cs
+++ TransIT.DAL/Repositories/ImplementedRepositories/SupplierRepository.cs
@@ -5,7 +5,7 @@ using TransIT.DAL.Repositories.InterfacesRepositories;
 
 namespace TransIT.DAL.Repositories.ImplementedRepositories
 {
-    public class SupplierRepository: BaseRepository<Supplier>, ISupplierRepository
+    public class SupplierRepository : BaseRepository<Supplier>, ISupplierRepository
     {
         public SupplierRepository(DbContext context)
                : base(context)
@@ -14,6 +14,9 @@ namespace TransIT.DAL.Repositories.ImplementedRepositories
 
         protected override IQueryable<Supplier> ComplexEntities => Entities.
                    Include(t => t.Create).
-                   Include(z => z.Mod).OrderByDescending(u => u.ModDate).ThenByDescending(x => x.CreateDate);
+                   Include(z => z.Mod).
+                   Include(c => c.Currency).
+                   Include(cc => cc.Country).
+                    OrderByDescending(u => u.ModDate).ThenByDescending(x => x.CreateDate);
     }
 }
