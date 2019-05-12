ed85280c6cdea2b3e5d6a53a28dbaccdcc56e747
markiyanshvets <markiyan.shvets@gmail.com>
2019-05-12 16:48:30 +0300

    automappers changed

---
+++ TransIT.DAL/Models/Mappings/CountryProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using TransIT.DAL.Models.DTOs;
+using TransIT.DAL.Models.Entities;
+
+namespace TransIT.DAL.Models.Mappings
+{
+    public class CountryProfile : Profile
+    {
+        public CountryProfile()
+        {
+            CreateMap<CountryDTO, Country>()
+                .ForMember(m => m.ModId, opt => opt.Ignore())
+                .ForMember(m => m.CreateId, opt => opt.Ignore());
+            CreateMap<Country, CountryDTO>();
+        }
+    }
+}
