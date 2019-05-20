b1a672534fdabb6904301ebf0e3fd83d7e2d675f
maksym-yaremko <maksym.yaremko@gmail.com>
2019-05-20 17:24:48 +0300

    fix entit (#169)

---
+++ TransIT.DAL/Models/Entities/Transition.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TransIT.DAL.Models.Entities
+{
+    public partial class Transition
+    {
+        public int Id { get; set; }
+        public int FromStateId { get; set; }
+        public int ToStateId { get; set; }
+        public int ActionTypeId { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public DateTime? ModDate { get; set; }
+        public int? CreateId { get; set; }
+        public int? ModId { get; set; }
+
+        public virtual ActionType ActionType { get; set; }
+        public virtual User Create { get; set; }
+        public virtual State FromState { get; set; }
+        public virtual User Mod { get; set; }
+        public virtual State ToState { get; set; }
+    }
+}
