8ef7557153fa68e75a99908819c88cfa080df850
Danylo Dudok <danylo.dudok@protonmail.com>
2019-05-28 16:24:33 +0300

    group query

--- TransIT.BLL/Services/StatisticService.cs
+++ TransIT.BLL/Services/StatisticService.cs
@@ -1,17 +1,28 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using TransIT.DAL.Models.Entities;
+using TransIT.DAL.Models.ViewModels;
 using TransIT.DAL.Repositories.InterfacesRepositories;
 
 namespace TransIT.BLL.Services
 {
     public class StatisticService
     {
-        protected readonly IIssueRepository _issueRepository;
-        protected readonly IVehicleTypeRepository _vehicleTypeRepository;
-        protected readonly IVehicleRepository _vehicleRepository;
-        protected readonly IMalfunctionRepository _malfunctionRepository;
-        protected readonly IMalfunctionGroupRepository _malfunctionGroupRepository;
-        protected readonly IMalfunctionSubgroupRepository _malfunctionSubGroupRepository;
+        protected readonly IQueryable<Issue> _issues;
+        protected readonly IQueryable<VehicleType> _vehicleTypes;
+        protected readonly IQueryable<Vehicle> _vehicles;
+        protected readonly IQueryable<Malfunction> _malfunctions;
+        protected readonly IQueryable<MalfunctionGroup> _malfunctionGroups;
+        protected readonly IQueryable<MalfunctionSubgroup> _malfunctionSubgroups;
+        
+//        protected readonly IIssueRepository _issueRepository;
+//        protected readonly IVehicleTypeRepository _vehicleTypeRepository;
+//        protected readonly IVehicleRepository _vehicleRepository;
+//        protected readonly IMalfunctionRepository _malfunctionRepository;
+//        protected readonly IMalfunctionGroupRepository _malfunctionGroupRepository;
+//        protected readonly IMalfunctionSubgroupRepository _malfunctionSubGroupRepository;
 
         public StatisticService(
             IIssueRepository issueRepository,
@@ -21,35 +32,42 @@ namespace TransIT.BLL.Services
             IMalfunctionGroupRepository malfunctionGroupRepository,
             IMalfunctionSubgroupRepository malfunctionSubGroupRepository)
         {
-            _issueRepository = issueRepository;
-            _vehicleTypeRepository = vehicleTypeRepository;
-            _malfunctionGroupRepository = malfunctionGroupRepository;
-            _malfunctionSubGroupRepository = malfunctionSubGroupRepository;
+            _issues = issueRepository.GetQueryable();
+            _vehicles = vehicleRepository.GetQueryable();
+            _vehicleTypes = vehicleTypeRepository.GetQueryable();
+            _malfunctions = malfunctionRepository.GetQueryable();
+            _malfunctionSubgroups = malfunctionSubGroupRepository.GetQueryable();
+            _malfunctionGroups = malfunctionGroupRepository.GetQueryable();
+
+//            _issueRepository = issueRepository;
+//            _vehicleTypeRepository = vehicleTypeRepository;
+//            _malfunctionGroupRepository = malfunctionGroupRepository;
+//            _malfunctionSubGroupRepository = malfunctionSubGroupRepository;
         }
 
-        public void GetStatisticGroup()
-        {
-            var issues = _issueRepository.GetQueryable();
-            var vehicles = _vehicleRepository.GetQueryable();
-            var vehicleTypes = _vehicleTypeRepository.GetQueryable();
-            var malfunctions = _malfunctionRepository.GetQueryable();
-            var malfunctionSubgroups = _malfunctionSubGroupRepository.GetQueryable();
-            var malfunctionGroups = _malfunctionGroupRepository.GetQueryable();
-
-            var res =
-                from i in issues
-                join v in vehicles on i.VehicleId equals v.Id
-                join vt in vehicleTypes on v.VehicleTypeId equals vt.Id
-                join m in malfunctions on i.MalfunctionId equals m.Id
-                join ms in malfunctionSubgroups on m.MalfunctionSubgroupId equals ms.Id
-                join mg in malfunctionGroups on ms.MalfunctionGroupId equals mg.Id
-                select new
-                {
-                    vt, mg, i
-                } into joined
-                group joined by new { malfunctionGroup = joined.mg.Id, vehicleType = joined.vt.Id } into grouped
-                select grouped;
-
+//        public void GetStatisticGroup()
+//        {
+//            var issues = _issueRepository.GetQueryable();
+//            var vehicles = _vehicleRepository.GetQueryable();
+//            var vehicleTypes = _vehicleTypeRepository.GetQueryable();
+//            var malfunctions = _malfunctionRepository.GetQueryable();
+//            var malfunctionSubgroups = _malfunctionSubGroupRepository.GetQueryable();
+//            var malfunctionGroups = _malfunctionGroupRepository.GetQueryable();
+//
+//            var res =
+//                from i in issues
+//                join v in vehicles on i.VehicleId equals v.Id
+//                join vt in vehicleTypes on v.VehicleTypeId equals vt.Id
+//                join m in malfunctions on i.MalfunctionId equals m.Id
+//                join ms in malfunctionSubgroups on m.MalfunctionSubgroupId equals ms.Id
+//                join mg in malfunctionGroups on ms.MalfunctionGroupId equals mg.Id
+//                select new
+//                {
+//                    vt, mg, i
+//                } into joined
+//                group joined by new { malfunctionGroup = joined.mg.Id, vehicleType = joined.vt.Id } into grouped
+//                select grouped;
+//
             
            // _issueRepository.GetQueryable().GroupBy().Where(issue =>
            //     _vehicleTypeRepository.GetQueryable().Where(type =>
@@ -58,34 +76,74 @@ namespace TransIT.BLL.Services
            //            && issue.Malfunction.MalfunctionSubgroup.MalfunctionGroupId == group.Id)
            //    )
            //)
-        }
+//        }
 
-        public void GetStatisticSubGroup(int groupId)
-        {
-            var issues = _issueRepository.GetQueryable();
-            var vehicles = _vehicleRepository.GetQueryable();
-            var vehicleTypes = _vehicleTypeRepository.GetQueryable();
-            var malfunctions = _malfunctionRepository.GetQueryable();
-            var malfunctionSubgroups = _malfunctionSubGroupRepository.GetQueryable();
-            var malfunctionGroups = _malfunctionGroupRepository.GetQueryable();
+//        public void GetStatisticSubGroup(int groupId)
+//        {
+//            var issues = _issueRepository.GetQueryable().AsNoTracking();
+//            var vehicles = _vehicleRepository.GetQueryable().AsNoTracking();
+//            var vehicleTypes = _vehicleTypeRepository.GetQueryable().AsNoTracking();
+//            var malfunctions = _malfunctionRepository.GetQueryable().AsNoTracking();
+//            var malfunctionSubgroups = _malfunctionSubGroupRepository.GetQueryable().AsNoTracking();
+//            var malfunctionGroups = _malfunctionGroupRepository.GetQueryable().AsNoTracking();
+//
+//            var joined =
+//                from i in issues
+//                join v in vehicles on i.VehicleId equals v.Id
+//                join vt in vehicleTypes on v.VehicleTypeId equals vt.Id
+//                join m in malfunctions on i.MalfunctionId equals m.Id
+//                join ms in malfunctionSubgroups on m.MalfunctionSubgroupId equals ms.Id
+//                join mg in malfunctionGroups on ms.MalfunctionGroupId equals mg.Id
+//                select new { vt, mg, i };
+//            
+//            var counted =
+//                from j in joined
+//                group j by new { malfunctionGroup = j.mg.Id, vehicleType = j.vt.Id } into g
+//                select new
+//                {
+//                    Count =
+//                        (from i in issues
+//                        where i.Malfunction.MalfunctionSubgroup.MalfunctionGroupId == g.Key.malfunctionGroup
+//                        select i).LongCount(),
+//                    VehicleType = g.Key.vehicleType,
+//                    MalfunctionGroup = g.Key.malfunctionGroup
+//                };
+//        }
 
-            var res =
-                 from i in issues
-                 join v in vehicles on i.VehicleId equals v.Id
-                 join vt in vehicleTypes on v.VehicleTypeId equals vt.Id
-                 join m in malfunctions on i.MalfunctionId equals m.Id
-                 join ms in malfunctionSubgroups on m.MalfunctionSubgroupId equals ms.Id
-                 join mg in malfunctionGroups on ms.MalfunctionGroupId equals mg.Id
-                 select new
-                 {
-                     vt,
-                     mg,
-                     i
-                 } into joined
-                 group joined by new { malfunctionGroup = joined.mg.Id, vehicleType = joined.vt.Id } into grouped
-                 select new { };
+//        public dynamic GetStatisticSubGroup(int groupId) =>
+//            from j in from i in _issues
+//                join v in _vehicles on i.VehicleId equals v.Id
+//                join vt in _vehicleTypes on v.VehicleTypeId equals vt.Id
+//                join m in _malfunctions on i.MalfunctionId equals m.Id
+//                join ms in _malfunctionSubgroups on m.MalfunctionSubgroupId equals ms.Id
+//                join mg in _malfunctionGroups on ms.MalfunctionGroupId equals mg.Id
+//                select new { vt, mg, i }
+//            group j by new { malfunctionGroup = j.mg.Id, vehicleType = j.vt.Id } into g
+//            select new
+//            {
+//                Count =
+//                    (from i in _issues
+//                        where i.Malfunction.MalfunctionSubgroup.MalfunctionGroupId == g.Key.malfunctionGroup
+//                        select i).LongCount(),
+//                VehicleType = g.Key.vehicleType,
+//                MalfunctionGroup = g.Key.malfunctionGroup
+//            };
 
-        }
-
+        public dynamic GetStatisticGroup() =>
+            from j in from i in _issues
+                join vt in _vehicleTypes on i.Vehicle.VehicleTypeId equals vt.Id
+                join mg in _malfunctionGroups on i.Malfunction.MalfunctionSubgroup.MalfunctionGroupId equals mg.Id
+                select new { vt, mg, i }
+            group j by new { malfunctionGroup = j.mg.Id, vehicleType = j.vt.Id } into g
+            select new VehicleTypeMalfunctionGroup
+            {
+                VehicleType = _vehicleTypes.FirstOrDefault(x => x.Id == g.Key.vehicleType),
+                GroupCount = (
+                    _malfunctionGroups.FirstOrDefault(x => x.Id == g.Key.malfunctionGroup),
+                    (ulong)(from i in _issues
+                            where i.Malfunction.MalfunctionSubgroup.MalfunctionGroupId == g.Key.malfunctionGroup
+                            select i).LongCount()
+                    )
+            };
     }
 }
--- TransIT.DAL/Models/ViewModels/VehicleTypeMalfunctionGroupViewModel.cs
+++ TransIT.DAL/Models/ViewModels/VehicleTypeMalfunctionGroupViewModel.cs
@@ -1,7 +1,14 @@
 ﻿using TransIT.DAL.Models.DTOs;
+using TransIT.DAL.Models.Entities;
 
 namespace TransIT.DAL.Models.ViewModels
 {
+    public class VehicleTypeMalfunctionGroup
+    {
+        public VehicleType VehicleType { get; set; }
+        public (MalfunctionGroup group, ulong count) GroupCount { get; set; }
+    }
+
     public class VehicleTypeMalfunctionGroupViewModel
     {
         public VehicleTypeDTO VehicleType { get; set; }
