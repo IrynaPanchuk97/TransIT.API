a1bcbb4f2d118ffc3c6177fc93797737597cb8e5
yuriimochulskyi <yurii.mochulskyi@gmail.com>
2019-05-29 16:56:50 +0300

    add swagger

---
+++ TransIT.API/Extensions/SwaggerExtension.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TransIT.API.Extensions
+{
+    public static class SwaggerExtension
+    {
+        public static void ConfigureSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+                {
+                    c.SwaggerDoc("v1", new Info
+                    {
+                        Version = "v1",
+                        Title = "TransIT API"
+                    });
+                    var security = new Dictionary<string, IEnumerable<string>>
+                    {
+                    {"Bearer", new string[] { }},
+                    };
+
+                    c.AddSecurityDefinition("Bearer", new ApiKeyScheme
+                    {
+                        Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                        Name = "Authorization",
+                        In = "header",
+                        Type = "apiKey"
+                    });
+                    c.AddSecurityRequirement(security);
+                    c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+                });
+        }
+    }
+}
