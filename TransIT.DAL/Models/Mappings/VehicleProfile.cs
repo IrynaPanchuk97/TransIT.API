9d66968d5137272efa57f07d5cfdfc791c2f4812
yuriimochulskyi <yurii.mochulskyi@gmail.com>
2019-06-03 01:21:48 +0300

    add mappings

--- TransIT.DAL/Models/Mappings/VehicleProfile.cs
+++ TransIT.DAL/Models/Mappings/VehicleProfile.cs
@@ -17,7 +17,9 @@ namespace TransIT.DAL.Models.Mappings
                 .ForMember(v => v.VehicleTypeId, opt => opt.MapFrom(x => x.VehicleType.Id))
                 .ForMember(v => v.VehicleType, opt => opt.Ignore())
                 .ForMember(v => v.ModDate, opt => opt.Ignore())
-                .ForMember(v => v.CreateDate, opt => opt.Ignore());
+                .ForMember(v => v.CreateDate, opt => opt.Ignore())
+                .ForMember(v => v.LocationId, opt => opt.MapFrom(x => x.Location.Id))
+                .ForMember(v => v.Location, opt => opt.Ignore());
             CreateMap<Vehicle, VehicleDTO>();
         }
     }
