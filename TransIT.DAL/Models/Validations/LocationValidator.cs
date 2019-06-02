7c632f2b1f4a7d5dbb9abbea298d4b7e45c2fad2
yuriimochulskyi <yurii.mochulskyi@gmail.com>
2019-06-03 01:29:02 +0300

    add validator

---
+++ TransIT.DAL/Models/Validations/LocationValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TransIT.DAL.Models.DTOs;
+
+namespace TransIT.DAL.Models.Validations
+{
+    public class LocationValidator : AbstractValidator<LocationDTO>
+    {
+        public LocationValidator()
+        {
+            RuleFor(t => t.Name)
+                .NotNull()
+                .NotEmpty();
+        }
+    }
+}
