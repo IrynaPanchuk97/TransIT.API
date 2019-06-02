be50e023f59d3138f80a4ad3d03b22384fe81d0a
yuriimochulskyi <yurii.mochulskyi@gmail.com>
2019-06-03 02:22:20 +0300

    add controllers

---
+++ TransIT.API/Controllers/LocationController.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TransIT.BLL.Services;
+using TransIT.BLL.Services.Interfaces;
+using TransIT.DAL.Models.DTOs;
+using TransIT.DAL.Models.Entities;
+
+namespace TransIT.API.Controllers
+{
+    [Authorize(Roles = "ADMIN,ENGINEER,ANALYST")]
+    public class LocationController : DataController<Location, LocationDTO>
+    {
+        private readonly ILocationService _locationService;
+        public LocationController(
+            IMapper mapper,
+            ILocationService locationService,
+            IFilterService<Location> odService
+            ) : base(mapper, locationService, odService)
+        {
+            _locationService = locationService;
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "ADMIN")]
+        public override Task<IActionResult> Create([FromBody] LocationDTO obj)
+        {
+            return base.Create(obj);
+        }
+
+        [HttpPut("{id}")]
+        [Authorize(Roles = "ADMIN")]
+        public override Task<IActionResult> Update(int id, [FromBody] LocationDTO obj)
+        {
+            return base.Update(id, obj);
+        }
+
+        [HttpDelete("{id}")]
+        [Authorize(Roles = "ADMIN")]
+        public override Task<IActionResult> Delete(int id)
+        {
+            return base.Delete(id);
+        }
+    }
+}
--- TransIT.DAL/Models/Entities/Location.cs
+++ TransIT.DAL/Models/Entities/Location.cs
@@ -4,7 +4,7 @@ using TransIT.DAL.Models.Entities.Abstractions;
 
 namespace TransIT.DAL.Models.Entities
 {
-    public partial class Location: IEntity;
+    public partial class Location: IEntity
     {
         public Location()
         {
