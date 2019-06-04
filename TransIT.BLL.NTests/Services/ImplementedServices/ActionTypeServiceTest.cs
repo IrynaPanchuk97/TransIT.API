18385d276972422ea7bc5c15e3afbcb01b782d26
markiyanshvets <markiyan.shvets@gmail.com>
2019-06-04 22:49:34 +0300

    first nunit test

---
+++ TransIT.BLL.NTests/Services/ImplementedServices/ActionTypeServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TransIT.BLL.Services.ImplementedServices;
+using TransIT.DAL.Models.Entities;
+using TransIT.DAL.Repositories.InterfacesRepositories;
+
+namespace TransIT.BLL.NTests.Services.ImplementedServices
+{
+    public class ActionTypeServiceTest : CrudServiceTest<ActionType>
+    {
+        protected override void InitializeService()
+        {
+            var mock = _repository.As<IActionTypeRepository>();
+            _crudService = new ActionTypeService(_unitOfWork.Object, _logger.Object, mock.Object);
+
+        }
+    }
+}
