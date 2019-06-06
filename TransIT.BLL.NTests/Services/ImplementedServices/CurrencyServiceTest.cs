ec9057fbde6a3db433099528c0c15c243c47747b
markiyanshvets <markiyan.shvets@gmail.com>
2019-06-06 17:39:15 +0300

    +3entity test

---
+++ TransIT.BLL.NTests/Services/ImplementedServices/CurrencyServiceTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TransIT.BLL.NTests.Services.ImplementedServices
+{
+    class CurrencyServiceTest
+    {
+    }
+}
